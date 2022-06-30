ec8e36417430959bce9625ebb752c9c692879209
Riyas <riyaske@gmail.com>
2022-06-30 08:17:51 +0530

    Added branch

---
+++ src/Component/SampleReact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class SampleReact extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default SampleReact
